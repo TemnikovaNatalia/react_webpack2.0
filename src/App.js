9d84215fa3262d50ead108995210ec0ed5eefbd3
Natalia <natellaozabota@gmail.com>
2022-02-16 19:34:27 +0300

    New version Lesson1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './App.css';
+import { Message } from './components/Message/Message.jsx';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Мое первое приложение
+        <h3>Привет, Анатолий</h3>
+        <Message
+          name={'Анатолий'}
+          help={'Игорю Ивановичу'}
+          phone={'+7 777 777 77 77'}
+        />
+      </header>
+    </div>
+  );
+}
